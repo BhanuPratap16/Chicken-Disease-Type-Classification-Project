62901af7c85abb151d609008a4af3248dd3b38c6
BhanuPratap16 <bhanupratap16902@gmail.com>
2024-08-14 12:10:20 +0530

    call backs added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.config.configuration import ConfigurationManager
+from cnnClassifier.components.prepare_call_backs import PrepareCallback
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -23,6 +25,19 @@ if __name__ == '__main__':
         obj = PrepareBaseModelTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+    
+STAGE_NAME="Prepare Call Backs"
+
+if __name__ == '__main__':
+    try:
+        config = ConfigurationManager()
+        prepare_callbacks_config = config.get_prepare_callback_config()
+        prepare_callbacks = PrepareCallback(config=prepare_callbacks_config)
+        callback_list = prepare_callbacks.get_tb_ckpt_callbacks()
     except Exception as e:
         logger.exception(e)
         raise e
