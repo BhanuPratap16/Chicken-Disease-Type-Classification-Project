195d46d6fe955676c8f6da4f270004bb6df40394
BhanuPratap16 <bhanupratap16902@gmail.com>
2024-08-15 11:00:08 +0530

    Training add

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTraining
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cnnClassifier.config.configuration import ConfigurationManager
 from cnnClassifier.components.prepare_call_backs import PrepareCallback
+from cnnClassifier.pipeline.stage_03_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
+
+import sys
+import io
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
@@ -18,26 +24,39 @@ except Exception as e:
 
 
 STAGE_NAME = "Prepare Base Model"
-if __name__ == '__main__':
-    try:
-        logger.info(f"*******************")
-        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = PrepareBaseModelTrainingPipeline()
-        obj.main()
-        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-    except Exception as e:
-        logger.exception(e)
-        raise e
-    
-    
-STAGE_NAME="Prepare Call Backs"
 
-if __name__ == '__main__':
-    try:
-        config = ConfigurationManager()
-        prepare_callbacks_config = config.get_prepare_callback_config()
-        prepare_callbacks = PrepareCallback(config=prepare_callbacks_config)
-        callback_list = prepare_callbacks.get_tb_ckpt_callbacks()
-    except Exception as e:
-        logger.exception(e)
-        raise e
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+    
+    
+# STAGE_NAME="Prepare Call Backs"
+
+# if __name__ == '__main__':
+#     try:
+#         config = ConfigurationManager()
+#         prepare_callbacks_config = config.get_prepare_callback_config()
+#         prepare_callbacks = PrepareCallback(config=prepare_callbacks_config)
+#         callback_list = prepare_callbacks.get_tb_ckpt_callbacks()
+#     except Exception as e:
+#         logger.exception(e)
+#         raise e
+
+STAGE_NAME = "Training stage"
+
+
+
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
